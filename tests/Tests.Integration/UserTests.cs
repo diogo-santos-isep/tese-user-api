92b5f645a1479fc8b80930be5328051c79b41fe7
Diogo Santos <1140294@isep.ipp.pt>
2020-09-10 16:40:26 +0100

    UC01- fixed integration tests

--- tests/Tests.Integration/UserTests.cs
+++ tests/Tests.Integration/UserTests.cs
@@ -4,7 +4,9 @@ namespace Tests.Integration
     using BLL.Services.Implementations;
     using DAL.Repositories.Implementations;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Models.Domain.Enums;
     using Models.Domain.Models;
+    using Models.Filters;
     using MongoDB.Bson;
     using System;
     using System.Linq;
@@ -28,7 +30,7 @@ namespace Tests.Integration
             var newUser = this.GenerateNewUser();
 
             newUser = this._service.Create(newUser);
-            Assert.IsFalse(String.IsNullOrEmpty(newUser.Id),"Id came back null or empty");
+            Assert.IsFalse(String.IsNullOrEmpty(newUser.Id), "Id came back null or empty");
 
             var savedUser = this._service.Get(newUser.Id);
             Assert.AreEqual(savedUser, newUser, "Users are different");
@@ -37,13 +39,29 @@ namespace Tests.Integration
         [TestMethod()]
         public void CreateAdminIfNotExists_Success()
         {
-            Assert.Fail();
+            DeleteAllUsers();
+            var newUser = this.GenerateNewUser();
+            newUser.Role = ERole.Admin;
+
+            this._service.CreateAdminIfNoUserExists();
+
+            var existingUsers = this._service.Get();
+            Assert.AreEqual(1, existingUsers.Count, "Não existe apenas o administrador");
+            Assert.AreEqual(User.GetAdminDefault().Email, existingUsers.First().Email, "Utilizador não é o administrador criado");
         }
 
         [TestMethod()]
         public void CreateAdminIfNotExists_AdminExists_NotCreated()
         {
-            Assert.Fail();
+            DeleteAllUsers();
+            var newUser = this.GenerateNewUser();
+            newUser.Role = ERole.Admin;
+            newUser = this._service.Create(newUser);
+
+            this._service.CreateAdminIfNoUserExists();
+
+            var existingUsers = this._service.Get();
+            Assert.AreEqual(1, existingUsers.Count, "Não existe apenas o administrador");
         }
 
         [TestMethod()]
@@ -55,7 +73,7 @@ namespace Tests.Integration
 
             this._service.Delete(newUser.Id);
             var savedUser = this._service.Get(newUser.Id);
-            Assert.IsNull(savedUser,"User is not null, therefore still exists");
+            Assert.IsNull(savedUser, "User is not null, therefore still exists");
         }
 
         [TestMethod()]
@@ -76,18 +94,30 @@ namespace Tests.Integration
             var newUser = this.GenerateNewUser();
             var newUser2 = this.GenerateNewUser();
 
-             this._service.Create(newUser);
-             this._service.Create(newUser2);
+            this._service.Create(newUser);
+            this._service.Create(newUser2);
 
             var users = this._service.Get();
-            Assert.IsTrue(users.Any(t=>t.Id == newUser.Id), $"User1 does not exist");
-            Assert.IsTrue(users.Any(t=>t.Id == newUser.Id), $"User2 does not exist");
+            Assert.IsTrue(users.Any(t => t.Id == newUser.Id), $"User1 does not exist");
+            Assert.IsTrue(users.Any(t => t.Id == newUser.Id), $"User2 does not exist");
         }
 
         [TestMethod()]
         public void Search_Success()
         {
-            Assert.Fail();
+            var newUser = this.GenerateNewUser();
+            var newUser2 = this.GenerateNewUser();
+
+            this._service.Create(newUser);
+            this._service.Create(newUser2);
+
+            var grid = this._service.Search(new UserFilter
+            {
+                Page = 1,
+                PageSize = 50
+            });
+            Assert.IsTrue(grid.List.Any(t => t.Id == newUser.Id), $"User1 does not exist");
+            Assert.IsTrue(grid.List.Any(t => t.Id == newUser.Id), $"User2 does not exist");
         }
 
         [TestMethod()]
@@ -107,8 +137,14 @@ namespace Tests.Integration
         {
             return this.fixture.Build<User>()
                 .Without(t => t.Id)
-                .With(t=>t.Department_Id,ObjectId.GenerateNewId().ToString())
+                .With(t => t.Department_Id, ObjectId.GenerateNewId().ToString())
                 .Create();
+        }
+
+        private void DeleteAllUsers()
+        {
+            this._service.Get()
+                .ForEach(u => this._service.Delete(u.Id));
         }
     }
 }
